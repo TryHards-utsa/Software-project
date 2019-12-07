99b7816cbd406605b0fd48f81467a59ca6c1afa6
KAY LAPTOP <KAY LAPTOP@LAPTOP-T1BKS2FC.attlocal.net>
2019-12-06 18:50:44 -0600

    updating models

--- src/application/model/Customer.java
+++ src/application/model/Customer.java
@@ -3,6 +3,13 @@ package application.model;
 import java.util.ArrayList;
 
 public class Customer {
-	String name;
-	ArrayList<items> cart;
+	String customerName;
+	String address;
+	ArrayList<Item> cart;
+	
+	public Customer(String customerName, String address, ArrayList<Item> cart){
+		this.customerName=customerName;
+		this.address=address;
+		this.cart=cart;
+	}
 }
--- src/application/model/Item.java
+++ src/application/model/Item.java
@@ -3,5 +3,31 @@ package application.model;
 import java.util.ArrayList;
 
 public class Item {
-	String name;
+	String itemName;
+	String price;
+	
+	//edit comments
+
+	public Item(String itemName, String price){
+		this.itemName=itemName;
+		this.price=price;
+	}
+	
+	public String toString(){ 
+		String ret ="-"+itemName+"------"+price;
+		return ret;
+	}
+	public String getItemName() {
+		return this.itemName;
+	}
+	public void setItemName(String itemName){
+		this.itemName=itemName;
+	}
+	
+	public String getPrice() {
+		return this.price;
+	}
+	public void setPrice(String price){
+		this.price=price;
+	}	
 }

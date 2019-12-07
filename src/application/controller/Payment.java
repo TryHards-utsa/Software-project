1957df39e19ec531c624d7db14efb6d44c7bb7be
chris <chris@10.224.176.159>
2019-12-06 20:59:03 -0600

    added some payment functionality

--- src/application/controller/Payment.java
+++ src/application/controller/Payment.java
@@ -2,14 +2,22 @@ package application.controller;
 
 import application.Main;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 
 public class Payment {
+	@FXML private TextField cardNumber;
+    @FXML private TextField securityCode;
 
 	
 	public void driverassignhandle(ActionEvent event) {
+		//get cc info and save it to 
+		int cardNum = Integer.parseInt( cardNumber.getText() );
+		int securityNum = Integer.parseInt( securityCode.getText() );
+		
 		try {
 			Parent root = FXMLLoader.load( getClass().getResource( "../view/DriverAssign.fxml" ) ); 
 			Scene scene = new Scene( root, 600, 600 );

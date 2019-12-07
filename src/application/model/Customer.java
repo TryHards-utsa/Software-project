96b9d5358d080a71b87c9fdfd6aec5fd36d8fe19
KAY LAPTOP <KAY LAPTOP@LAPTOP-T1BKS2FC.attlocal.net>
2019-12-06 19:14:00 -0600

    updates

--- src/application/model/Customer.java
+++ src/application/model/Customer.java
@@ -12,4 +12,28 @@ public class Customer {
 		this.address=address;
 		this.cart=cart;
 	}
+	public String toString(){ 
+		String ret =customerName+" thank you for your purchase!\n Your groceries are currently being sent to"+address;
+		ret = ret+"\n---YOUR RECIPT---\n"+cart.toString()+"\n Have a nice day!";
+		return ret;
+	}
+	public String getCutsomerName() {
+		return this.customerName;
+	}
+	public void setCutsomerName(String customerName){
+		this.customerName=customerName;
+	}
+	
+	public String getAddress() {
+		return this.address;
+	}
+	public void setAddress(String address){
+		this.address=address;
+	}	
+	public ArrayList<Item> getCart() {
+		return this.cart;
+	}
+	public void setCart(ArrayList<Item> cart){
+		this.cart=cart;
+	}
 }

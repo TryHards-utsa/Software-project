71e47e6fc1a551199c9f15e81e497a58487f0f13
chris <chris@10.224.176.159>
2019-12-07 16:56:29 -0600

    made cc info Strings for easier error handling

--- src/application/controller/Payment.java
+++ src/application/controller/Payment.java
@@ -7,25 +7,30 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class Payment {
 	@FXML private TextField cardNumber;
     @FXML private TextField securityCode;
+    @FXML Text error;
 
 	
 	public void driverassignhandle(ActionEvent event) {
 		//get cc info and save it to 
-		int cardNum = Integer.parseInt( cardNumber.getText() );
-		int securityNum = Integer.parseInt( securityCode.getText() );
-		
-		try {
-			Parent root = FXMLLoader.load( getClass().getResource( "../view/DriverAssign.fxml" ) ); 
-			Scene scene = new Scene( root, 600, 600 );
-			Main.stage.setScene(scene);
-			Main.stage.show();
-			
-		} catch(Exception e) {
-			e.printStackTrace();
+		String cardNum = cardNumber.getText();
+		String securityNum = securityCode.getText();
+		if( cardNum.length() != 19 || securityNum.length() != 3 ) {
+			error.setText("Enter Card Number: xxxx-xxxx-xxxx-xxxx and Security Code: xxx");
+		} else {
+			try {
+				Parent root = FXMLLoader.load( getClass().getResource( "../view/DriverAssign.fxml" ) ); 
+				Scene scene = new Scene( root, 600, 600 );
+				Main.stage.setScene(scene);
+				Main.stage.show();
+				
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	

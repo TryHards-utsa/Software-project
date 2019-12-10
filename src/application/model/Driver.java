ad1b3a47d0b6bb7ee25ab4c3ab9f71e4e0e3e4e5
john longoria <longoriajohn222@gmail.com>
2019-12-10 17:21:33 -0600

    Driver javadoc comments

--- src/application/model/Driver.java
+++ src/application/model/Driver.java
@@ -2,12 +2,18 @@ package application.model;
 
 import java.util.ArrayList;
 
+/**
+ * Delivery driver with a name and availability status
+ *
+ */
 public class Driver {
 	String driverName;
 	String status;
 	
-	//edit comments
-
+	/**
+	 * @param driverName the delivery driver
+	 * @param status availability of the driver
+	 */
 	public Driver(String driverName, String status){
 		this.driverName=driverName;
 		this.status=status;
@@ -18,16 +24,28 @@ public class Driver {
 		String ret ="-"+driverName+" is currently "+status;
 		return ret;
 	}
+	/**
+	 * @return name of current driver
+	 */
 	public String getDriverName() {
 		return this.driverName;
 	}
+	/**
+	 * @param driverName specified name of the driver
+	 */
 	public void setItemName(String driverName){
 		this.driverName=driverName;
 	}
 	
+	/**
+	 * @return status of a Delivery driver
+	 */
 	public String getStatus() {
 		return this.status;
 	}
+	/**
+	 * @param status current availability  of the driver
+	 */
 	public void setStatus(String status){
 		this.status=status;
 	}	

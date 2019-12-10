7bc76923b707a1e7029f430f42113a9087c46ae8
john longoria <longoriajohn222@gmail.com>
2019-12-10 17:09:18 -0600

    JavaDoc Comments

--- src/application/model/Customer.java
+++ src/application/model/Customer.java
@@ -2,6 +2,10 @@ package application.model;
 
 import java.util.ArrayList;
 
+/**
+ * Customer class represents a generic Customer with a name, address, cart and login credentials
+ *
+ */
 public class Customer {
 	String customerName;
 	String address;
@@ -9,6 +13,13 @@ public class Customer {
 	String email;
 	ArrayList<Item> cart;
 	
+	/**
+	 * @param customerName full name of a customer
+	 * @param address delivery location 
+	 * @param password login password
+	 * @param email customer email key
+	 * @param cart list of items for the customer
+	 */
 	public Customer(String customerName, String address, String password,String email, ArrayList<Item> cart){
 		this.customerName=customerName;
 		this.address=address;
@@ -16,6 +27,7 @@ public class Customer {
 		this.email=email;
 		this.cart=cart;
 	}
+	
 	public String toString(){ 
 		String ret =customerName+" thank you for your purchase!\n Your groceries are currently being sent to"+address;
 		ret = ret+"\n---YOUR RECIPT---\n";
@@ -25,10 +37,17 @@ public class Customer {
 		ret = ret+"\n Have a nice day!";
 		return ret;
 	}
+	/**
+	 * Adds the specified item to the cart
+	 * @param name the item key
+	 */
 	public void addItemToCart(Item name){
 		cart.add(name);
 		
 	}	
+	/**
+	 * @param name the item to key to remove
+	 */
 	public void removeItemFromCart(Item name){
 		int index;
 		for(index=0;index<cart.size();index++) {
@@ -38,33 +57,64 @@ public class Customer {
 		}
 	}
 	
+	/**
+	 * @return full name of customer
+	 */
 	public String getCutsomerName() {
 		return this.customerName;
 	}
+	/**
+	 * @param customerName full name of the customer
+	 */
 	public void setCutsomerName(String customerName){
 		this.customerName=customerName;
 	}
+	/**
+	 * @return address of the delivery location
+	 */
 	public String getAddress() {
 		return this.address;
 	}
+	/**
+	 * @param address the specified delivery location for the customer
+	 */
 	public void setAddress(String address){
 		this.address=address;
 	}
+	/**
+	 * @return the password key of user
+	 */
 	public String getPassword() {
 		return this.password;
 	}
+	/**
+	 * @param password the specified password to be assigned to
+	 */
 	public void setPassword(String password){
 		this.password=password;
 	}
+	/**
+	 * @return email key of Customer
+	 */
 	public String getEmail() {
 		return this.email;
 	}
+	/**
+	 * @param email the email key to be assigned to
+	 */
 	public void setEmails(String email){
 		this.email=email;
 	}		
+	/**
+	 * @return list of items for the customers
+	 */
 	public ArrayList<Item> getCart() {
 		return this.cart;
 	}
+	
+	/**
+	 * @param cart list of items to be added
+	 */
 	public void setCart(ArrayList<Item> cart){
 		this.cart=cart;
 	}

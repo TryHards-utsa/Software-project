7c7c93ab8cb33139572309f0d9d9c8682af97289
john longoria <longoriajohn222@gmail.com>
2019-12-09 23:36:37 -0600

    UnitTest nothing implemented

---
+++ src/application/test/DatasetTest.java
@@ -0,0 +1,50 @@
+package application.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DatasetTest {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAddCustomer() {
+		//TODO implement
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoadCustomer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoadItems() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCustomerList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetItemsList() {
+		fail("Not yet implemented");
+	}
+
+}

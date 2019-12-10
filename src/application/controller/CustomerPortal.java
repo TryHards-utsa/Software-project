eced8b06057f6398cc567da473087268fcbf98f9
cguerra95 <caleblguerra@gmail.com>
2019-12-10 16:27:38 -0600

    fxml change in store view

--- src/application/controller/CustomerPortal.java
+++ src/application/controller/CustomerPortal.java
@@ -56,7 +56,7 @@ public class CustomerPortal implements EventHandler<ActionEvent> {
 				if(startemail.getText().equals(customerList.getCustomerList().get(i).getEmail()) 
 						&& startpassword.getText().equals(customerList.getCustomerList().get(i).getPassword())) {
 					Parent root1 = FXMLLoader.load( getClass().getResource( "../view/StoreMenu.fxml" ) ); 
-					Scene scene1 = new Scene( root1, 600, 470 );
+					Scene scene1 = new Scene( root1, 600, 355 );
 					Main.stage.setScene(scene1);
 					Main.stage.show();
 				}else {

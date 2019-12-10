052919c42b6cd61ec4e359cc8bbd3db3eb1663dc
john longoria <longoriajohn222@gmail.com>
2019-12-10 17:16:18 -0600

    Item comments

--- src/application/model/Item.java
+++ src/application/model/Item.java
@@ -2,12 +2,19 @@ package application.model;
 
 import java.util.ArrayList;
 
+/**
+ * Item class represents a generic Item with a name key and price.
+ *
+ */
 public class Item {
 	String itemName;
 	double price;
 	
-	//edit comments
-
+	
+	/**
+	 * @param itemName name of the item
+	 * @param price the amount for a specified item
+	 */
 	public Item(String itemName, double price){
 		this.itemName=itemName;
 		this.price=price;
@@ -18,16 +25,30 @@ public class Item {
 		String ret ="-"+itemName+"------"+price;
 		return ret;
 	}
+	/**
+	 * @return name key of the item
+	 */
 	public String getItemName() {
 		return this.itemName;
 	}
+	/**
+	 * Assign the specified name to the item
+	 * @param itemName name of item to be assigned to
+	 */
 	public void setItemName(String itemName){
 		this.itemName=itemName;
 	}
 	
+	/**
+	 * @return price of unit item
+	 */
 	public double getPrice() {
 		return this.price;
 	}
+	
+	/**
+	 * @param price amount of the specified item to be set to
+	 */
 	public void setPrice(double price){
 		this.price=price;
 	}	
